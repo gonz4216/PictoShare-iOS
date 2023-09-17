67432b9da9c4452eacc4cbf79cea40c2c48699ba
Ethan Gonsalves <ethgon19@gmail.com>
2023-09-17 15:28:14 -0700

    Initial Commit

---
+++ PictoShare/PictoShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictoShareApp.swift
+//  PictoShare
+//
+//  Created by Ethan Gonsalves on 2023-09-17.
+//
+
+import SwiftUI
+
+@main
+struct PictoShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
