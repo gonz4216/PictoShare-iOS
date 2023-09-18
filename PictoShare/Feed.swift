426ee3851a0fe506cc21f9b074b37e711df0d367
Ethan Gonsalves <ethgon19@gmail.com>
2023-09-17 17:58:52 -0700

    first commit

---
+++ PictoShare/Feed.swift
@@ -0,0 +1,53 @@
+//
+//  Feed.swift
+//  PictoShare
+//
+//  Created by Ethan Gonsalves on 2023-09-17.
+//
+
+import SwiftUI
+
+struct Feed: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            ZStack {
+                VStack {
+                    VStack {
+                        HStack(){
+                            Image(systemName: "person.2.fill")
+                                .foregroundColor(.white)
+                            Spacer()
+                            Text("BeReal.")
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .font(.system(size: 22))
+                            Spacer()
+                            Image("pp")
+                                .resizable()
+                                .frame(width: 35, height: 35)
+                                .cornerRadius(17.5)
+                        }
+                        .padding(.horizontal)
+                        HStack {
+                            Text("My Friends")
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                            Text("Discovery")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    Spacer()
+                }
+            }
+        }
+
+    }
+}
+
+struct Feed_Previews: PreviewProvider {
+    static var previews: some View {
+        Feed()
+    }
+}
