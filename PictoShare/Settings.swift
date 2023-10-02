4bbb5fc02b5e823a40c8dd147c0109890678b9e5
Ethan Gonsalves <ethgon19@gmail.com>
2023-10-02 16:16:51 -0700

    m

---
+++ PictoShare/Settings.swift
@@ -0,0 +1,22 @@
+//
+//  Settings.swift
+//  PictoShare
+//
+//  Created by Ethan Gonsalves on 2023-10-02.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+        }
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
